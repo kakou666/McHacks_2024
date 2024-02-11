bf47ad2aa566d7ea8992916035a964817890c546
rimemokhtari <rimemokht@gmail.com>
2024-02-11 03:30:14 -0500

    dagDG

--- home.js
+++ home.js
@@ -2,7 +2,6 @@ let clickedBtnHome=document.getElementById("btnHome")
 
 clickedBtnHome.addEventListener('click',function(event){
   if(confirm("Make sure you saved your flashcards before exiting! If not click on 'Cancel' and save them")){
-    window.location.href="home.html"
-  }else{
+    window.open("home.html")
   }
 })
